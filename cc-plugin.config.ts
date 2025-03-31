4c750951cbd33ef40e26a23d80fe9412608401e9
xu_yanfeng <xu_yanfeng@126.com>
2025-03-31 10:57:58 +0800

    增加快捷键，方便快速打开调试面板

--- cc-plugin.config.ts
+++ cc-plugin.config.ts
@@ -17,7 +17,7 @@ const manifest: CocosPluginManifest = {
       main: "./src/panel/index.ts",
       title: "mcp-tools",
       width: 700,
-      height: 900,
+      height: 400,
       minWidth: 50,
       minHeight: 400,
     },
@@ -26,11 +26,13 @@ const manifest: CocosPluginManifest = {
     {
       valid: true,
       path: `i18n.title`,
+      accelerator: "CmdOrCtrl+M",
       message: {
         name: "showPanel",
       },
     },
   ],
+  scene: "./src/scene.ts",
   i18n_en: "./src/i18n/en.ts",
   i18n_zh: "./src/i18n/zh.ts",
   messages: ["getMcpTools", "runMcpTool"],
